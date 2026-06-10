efc2ecd749245fcbbd60145be9416b14c179bebc
Inaya Agil <inayaagil@gmail.com>
2026-06-10 13:54:13 +0700

    baru

--- OBATIN/view/FormAdmin.Designer.cs
+++ OBATIN/view/FormAdmin.Designer.cs
@@ -35,6 +35,7 @@
             this.kelolastok_btn = new System.Windows.Forms.Button();
             this.transaksi_btn = new System.Windows.Forms.Button();
             this.dashboard_btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.judul_lbl = new System.Windows.Forms.Label();
@@ -44,13 +45,12 @@
             this.stok_lbl = new System.Windows.Forms.Label();
             this.expired_panel = new System.Windows.Forms.Panel();
             this.expired_lbl = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
             this.transaksi_panel.SuspendLayout();
             this.stokobat_panel.SuspendLayout();
             this.expired_panel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -65,8 +65,9 @@
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(220, 563);
+            this.panel1.Size = new System.Drawing.Size(196, 450);
             this.panel1.TabIndex = 0;
             // 
             // logout_btn
@@ -74,9 +75,10 @@
             this.logout_btn.BackColor = System.Drawing.Color.DarkRed;
             this.logout_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.logout_btn.ForeColor = System.Drawing.Color.White;
-            this.logout_btn.Location = new System.Drawing.Point(12, 506);
+            this.logout_btn.Location = new System.Drawing.Point(11, 405);
+            this.logout_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.logout_btn.Name = "logout_btn";
-            this.logout_btn.Size = new System.Drawing.Size(200, 45);
+            this.logout_btn.Size = new System.Drawing.Size(178, 36);
             this.logout_btn.TabIndex = 6;
             this.logout_btn.Text = "Logout";
             this.logout_btn.UseVisualStyleBackColor = false;
@@ -85,9 +87,10 @@
             // 
             this.menuuser_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.menuuser_btn.ForeColor = System.Drawing.Color.Black;
-            this.menuuser_btn.Location = new System.Drawing.Point(15, 427);
+            this.menuuser_btn.Location = new System.Drawing.Point(13, 342);
+            this.menuuser_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.menuuser_btn.Name = "menuuser_btn";
-            this.menuuser_btn.Size = new System.Drawing.Size(190, 45);
+            this.menuuser_btn.Size = new System.Drawing.Size(169, 36);
             this.menuuser_btn.TabIndex = 5;
             this.menuuser_btn.Text = "Menu User";
             this.menuuser_btn.UseVisualStyleBackColor = true;
@@ -96,9 +99,10 @@
             // 
             this.riwayat_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.riwayat_btn.ForeColor = System.Drawing.Color.Black;
-            this.riwayat_btn.Location = new System.Drawing.Point(15, 350);
+            this.riwayat_btn.Location = new System.Drawing.Point(13, 280);
+            this.riwayat_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.riwayat_btn.Name = "riwayat_btn";
-            this.riwayat_btn.Size = new System.Drawing.Size(190, 45);
+            this.riwayat_btn.Size = new System.Drawing.Size(169, 36);
             this.riwayat_btn.TabIndex = 4;
             this.riwayat_btn.Text = "Riwayat /LogStok";
             this.riwayat_btn.UseVisualStyleBackColor = true;
@@ -107,9 +111,10 @@
             // 
             this.kelolastok_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.kelolastok_btn.ForeColor = System.Drawing.Color.Black;
-            this.kelolastok_btn.Location = new System.Drawing.Point(15, 274);
+            this.kelolastok_btn.Location = new System.Drawing.Point(13, 219);
+            this.kelolastok_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.kelolastok_btn.Name = "kelolastok_btn";
-            this.kelolastok_btn.Size = new System.Drawing.Size(190, 45);
+            this.kelolastok_btn.Size = new System.Drawing.Size(169, 36);
             this.kelolastok_btn.TabIndex = 3;
             this.kelolastok_btn.Text = "Kelola Stok Obat";
             this.kelolastok_btn.UseVisualStyleBackColor = true;
@@ -118,9 +123,10 @@
             // 
             this.transaksi_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.transaksi_btn.ForeColor = System.Drawing.Color.Black;
-            this.transaksi_btn.Location = new System.Drawing.Point(15, 198);
+            this.transaksi_btn.Location = new System.Drawing.Point(13, 158);
+            this.transaksi_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.transaksi_btn.Name = "transaksi_btn";
-            this.transaksi_btn.Size = new System.Drawing.Size(190, 45);
+            this.transaksi_btn.Size = new System.Drawing.Size(169, 36);
             this.transaksi_btn.TabIndex = 2;
             this.transaksi_btn.Text = "Transaksi Baru";
             this.transaksi_btn.UseVisualStyleBackColor = true;
@@ -129,22 +135,35 @@
             // 
             this.dashboard_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.dashboard_btn.ForeColor = System.Drawing.Color.Black;
-            this.dashboard_btn.Location = new System.Drawing.Point(15, 123);
+            this.dashboard_btn.Location = new System.Drawing.Point(13, 98);
+            this.dashboard_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dashboard_btn.Name = "dashboard_btn";
-            this.dashboard_btn.Size = new System.Drawing.Size(190, 45);
+            this.dashboard_btn.Size = new System.Drawing.Size(169, 36);
             this.dashboard_btn.TabIndex = 1;
             this.dashboard_btn.Text = "Dashboard";
             this.dashboard_btn.UseVisualStyleBackColor = true;
             this.dashboard_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::OBATIN.Properties.Resources.obatin;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(196, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.LightCyan;
             this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.judul_lbl);
-            this.panel2.Location = new System.Drawing.Point(219, 0);
+            this.panel2.Location = new System.Drawing.Point(195, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(635, 100);
+            this.panel2.Size = new System.Drawing.Size(564, 80);
             this.panel2.TabIndex = 1;
             // 
             // label1
@@ -152,9 +171,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Lucida Bright", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Teal;
-            this.label1.Location = new System.Drawing.Point(26, 56);
+            this.label1.Location = new System.Drawing.Point(23, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(294, 21);
+            this.label1.Size = new System.Drawing.Size(245, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Kelola data obat dengan mudah";
             // 
@@ -163,9 +182,9 @@
             this.judul_lbl.AutoSize = true;
             this.judul_lbl.Font = new System.Drawing.Font("Lucida Sans", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.judul_lbl.ForeColor = System.Drawing.Color.Teal;
-            this.judul_lbl.Location = new System.Drawing.Point(25, 24);
+            this.judul_lbl.Location = new System.Drawing.Point(22, 19);
             this.judul_lbl.Name = "judul_lbl";
-            this.judul_lbl.Size = new System.Drawing.Size(349, 32);
+            this.judul_lbl.Size = new System.Drawing.Size(291, 27);
             this.judul_lbl.TabIndex = 0;
             this.judul_lbl.Text = "Selamat Datang Admin";
             // 
@@ -173,9 +192,10 @@
             // 
             this.transaksi_panel.BackColor = System.Drawing.Color.CadetBlue;
             this.transaksi_panel.Controls.Add(this.transaksi_lbl);
-            this.transaksi_panel.Location = new System.Drawing.Point(251, 169);
+            this.transaksi_panel.Location = new System.Drawing.Point(223, 135);
+            this.transaksi_panel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.transaksi_panel.Name = "transaksi_panel";
-            this.transaksi_panel.Size = new System.Drawing.Size(156, 128);
+            this.transaksi_panel.Size = new System.Drawing.Size(139, 102);
             this.transaksi_panel.TabIndex = 2;
             // 
             // transaksi_lbl
@@ -183,9 +203,9 @@
             this.transaksi_lbl.AutoSize = true;
             this.transaksi_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.transaksi_lbl.ForeColor = System.Drawing.Color.White;
-            this.transaksi_lbl.Location = new System.Drawing.Point(4, 20);
+            this.transaksi_lbl.Location = new System.Drawing.Point(4, 16);
             this.transaksi_lbl.Name = "transaksi_lbl";
-            this.transaksi_lbl.Size = new System.Drawing.Size(149, 20);
+            this.transaksi_lbl.Size = new System.Drawing.Size(136, 17);
             this.transaksi_lbl.TabIndex = 0;
             this.transaksi_lbl.Text = "Transaksi Hari Ini";
             // 
@@ -193,9 +213,10 @@
             // 
             this.stokobat_panel.BackColor = System.Drawing.Color.CadetBlue;
             this.stokobat_panel.Controls.Add(this.stok_lbl);
-            this.stokobat_panel.Location = new System.Drawing.Point(667, 169);
+            this.stokobat_panel.Location = new System.Drawing.Point(593, 135);
+            this.stokobat_panel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.stokobat_panel.Name = "stokobat_panel";
-            this.stokobat_panel.Size = new System.Drawing.Size(156, 128);
+            this.stokobat_panel.Size = new System.Drawing.Size(139, 102);
             this.stokobat_panel.TabIndex = 3;
             // 
             // stok_lbl
@@ -203,9 +224,9 @@
             this.stok_lbl.AutoSize = true;
             this.stok_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.stok_lbl.ForeColor = System.Drawing.Color.White;
-            this.stok_lbl.Location = new System.Drawing.Point(32, 20);
+            this.stok_lbl.Location = new System.Drawing.Point(28, 16);
             this.stok_lbl.Name = "stok_lbl";
-            this.stok_lbl.Size = new System.Drawing.Size(95, 20);
+            this.stok_lbl.Size = new System.Drawing.Size(85, 17);
             this.stok_lbl.TabIndex = 0;
             this.stok_lbl.Text = "Stok Obat ";
             // 
@@ -213,9 +234,10 @@
             // 
             this.expired_panel.BackColor = System.Drawing.Color.CadetBlue;
             this.expired_panel.Controls.Add(this.expired_lbl);
-            this.expired_panel.Location = new System.Drawing.Point(453, 169);
+            this.expired_panel.Location = new System.Drawing.Point(403, 135);
+            this.expired_panel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.expired_panel.Name = "expired_panel";
-            this.expired_panel.Size = new System.Drawing.Size(166, 128);
+            this.expired_panel.Size = new System.Drawing.Size(148, 102);
             this.expired_panel.TabIndex = 4;
             // 
             // expired_lbl
@@ -223,39 +245,31 @@
             this.expired_lbl.AutoSize = true;
             this.expired_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.expired_lbl.ForeColor = System.Drawing.Color.White;
-            this.expired_lbl.Location = new System.Drawing.Point(46, 20);
+            this.expired_lbl.Location = new System.Drawing.Point(41, 16);
             this.expired_lbl.Name = "expired_lbl";
-            this.expired_lbl.Size = new System.Drawing.Size(69, 20);
+            this.expired_lbl.Size = new System.Drawing.Size(62, 17);
             this.expired_lbl.TabIndex = 0;
             this.expired_lbl.Text = "Expired";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::OBATIN.Properties.Resources.obatin;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(220, 100);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
             // FormAdmin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(854, 563);
+            this.ClientSize = new System.Drawing.Size(759, 450);
             this.Controls.Add(this.expired_panel);
             this.Controls.Add(this.stokobat_panel);
             this.Controls.Add(this.transaksi_panel);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.Color.Red;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormAdmin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dashboard Admin";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.transaksi_panel.ResumeLayout(false);
@@ -264,7 +278,6 @@
             this.stokobat_panel.PerformLayout();
             this.expired_panel.ResumeLayout(false);
             this.expired_panel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
